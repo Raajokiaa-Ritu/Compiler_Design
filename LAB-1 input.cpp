d292e9b62c41646fb1425a084196c00cab0daeb7
Khandaker Jannatul Ritu <45202439+Raajokiaa-Ritu@users.noreply.github.com>
2025-06-17 08:49:51 +0600

    Update LAB-1 input.cpp

--- LAB-1 input.cpp
+++ LAB-1 input.cpp
@@ -1,1 +1,18 @@
+📝 Sample input.cpp File
+int main() {
+    int x = 10;
+    float y = 3.14;
+
+    // This is a comment
+    /*
+       Multi-line
+       comment
+    */
+
+    if (x > 5) {
+        y = y + x;
+    }
+
+    return 0;
+}
 
